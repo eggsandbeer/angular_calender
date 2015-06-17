37471b4b48d8a8a92a39c70582a2be08bced4ea6
Cameron Strandberg <cameron.strandberg@gmail.com>
2015-06-16 22:13:19 -0700

    Calender now works.

--- app/calender/controller.js
+++ app/calender/controller.js
@@ -1,11 +1,8 @@
 var calender_controllers = angular.module('calenderDemo.controllers', []);
 
 calender_controllers.controller('mainController', function($scope, moment){
-  console.log('meow')
-  console.log(moment)
 
-
-  console.log($scope.day)
-
+  $scope.day = moment();
+  
 
 });
